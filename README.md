e3478d137e8e187dc70848112b9b942a571e7567
carissa-chandra <carissa.tjan03@gmail.com>
2026-09-11 15:33:13 +0700

    update footer

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Shopping Cart from The Odin Project (TOP) Course
+This **Shopping Cart** project is built for [The Odin Project](https://www.theodinproject.com/) assignment in [Project: Shopping Cart](https://www.theodinproject.com/lessons/node-path-react-new-shopping-cart).
+
+**Shopping Cart** is an front-end app that use  
+
+<font color="#ee26c3"><b>Tes</b></font>
+
+## Table of Contents
+- How to Use This Repository
+- 
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,16 @@
 const Footer = () => {
     return (
         <footer>
-            <p>Carissa Chandra</p>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                <h1>Shopping Cart</h1>
+                <p>&copy; 2026 Carissa Chandra</p>
+            </div>
+
+            <aside style={{ display: 'flex', flexDirection: 'column', gap: '8px' }}>
+                <h2 style={{ display: 'flex', height: '40px', alignItems: 'end' }}>Quick Links</h2>
+                <a href="/" style={{ color: 'var(--color-on-text)' }}>Home</a>
+                <a href="/cart" style={{ color: 'var(--color-on-text)' }}>Cart</a>
+            </aside>
         </footer>
     )
 };
